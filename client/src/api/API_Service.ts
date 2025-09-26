b6ed3ede64204e30ca87cb56f1895a3ad2448484
Bao Toan <baotoanit1505@gmail.com>
2025-09-26 22:22:57 +0700

    update

--- client/src/api/API_Service.ts
+++ client/src/api/API_Service.ts
@@ -2,7 +2,8 @@ import axios from "axios";
 
 export const API_Service = async (): Promise<Package[]> => {
   const res = await axios.get<Package[]>(
-    "http://192.168.192.189:8080/packages"
+    // "http://192.168.192.189:8080/packages"
+    "https://68d20c23cc7017eec5425e30.mockapi.io/service"
   );
   return res.data;
 };
